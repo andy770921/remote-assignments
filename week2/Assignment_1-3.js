939de83b94b623977fb7e8bb7d190c6c825925f2
andy770921 <andy770921@yahoo.com.tw>
2019-06-18 11:49:12 +0800

    Update Assignment_1-3.js

--- week2/Assignment_1-3.js
+++ week2/Assignment_1-3.js
@@ -49,3 +49,19 @@ calculate(obj2);
 
 //-----------------------------------------
 //Assignment 3: Function, Array, and Object
+// Complete the function below to calculate the average price of all the products.
+
+function avg(data){
+	// your code here
+}
+avg({
+	size:3,
+	products:[
+		{name:"Product 1", price:100},
+		{name:"Product 2", price:700},
+		{name:"Product 3", price:250}
+	]
+}); // show the average price of all products
+
+
+
