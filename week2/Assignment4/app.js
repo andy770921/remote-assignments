101fe0c90f1e93f0be1b98e82e0c2acbe5b4b51e
Andy Chou <andy770921@yahoo.com.tw>
2019-06-22 16:27:08 +0800

    finish manu_btn and cross btn switch function

--- week2/Assignment4/app.js
+++ week2/Assignment4/app.js
@@ -1,9 +1,11 @@
 const mainTitle = document.getElementById('mainTitle');
 const intermediate = document.querySelector('.intermediate');
+const manu_btn = document.querySelector('.manu_btn');
+const cross = document.querySelector('.cross');
 
 const resetCursor = (event) => {  
     event.target.style.cursor = "default";
-}
+};
 
 mainTitle.addEventListener('click', (event) => {
     mainTitle.textContent = 'Have a good time!';
@@ -13,4 +15,14 @@ mainTitle.addEventListener('click', (event) => {
 intermediate.addEventListener('click', (event) => {
     event.target.parentNode.nextElementSibling.style.display = "block";
     resetCursor(event);
+  });
+
+manu_btn.addEventListener('click', (event) => {
+    event.target.style.visibility = "hidden";
+    event.target.nextElementSibling.style.visibility = "visible";
+  });
+
+cross.addEventListener('click', (event) => {
+    event.target.style.visibility = "hidden";
+    event.target.previousElementSibling.style.visibility = "visible";
   });
