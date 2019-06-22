8f3fc15c4e1988f227a5e57e04ea8aed57a4a9ee
Andy Chou <andy770921@yahoo.com.tw>
2019-06-22 15:29:56 +0800

    finish function of extended 4-box button

--- week2/Assignment4/app.js
+++ week2/Assignment4/app.js
@@ -1,4 +1,5 @@
 const mainTitle = document.getElementById('mainTitle');
+const intermediate = document.querySelector('.intermediate');
 
 const resetCursor = (event) => {  
     event.target.style.cursor = "default";
@@ -6,5 +7,10 @@ const resetCursor = (event) => {
 
 mainTitle.addEventListener('click', (event) => {
     mainTitle.textContent = 'Have a good time!';
+    resetCursor(event);
+  });
+
+intermediate.addEventListener('click', (event) => {
+    event.target.parentNode.nextElementSibling.style.display = "block";
     resetCursor(event);
   });
