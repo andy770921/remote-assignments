6943f5e603c96fd9c02877069fd9a20635cdc92c
Andy Chou <andy770921@yahoo.com.tw>
2019-06-29 00:02:48 +0800

    install nodemon

--- week3/Assignment1/app.js
+++ week3/Assignment1/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response)=> {
-    response.send('Hello, My Server!');
+app.get('/', (req, res)=> {
+    res.send('Hello, My Server!');
   });
-app.listen(3000);
+  app.get('/getData', (req, res)=> {
+    res.send('Lack of Parameter');
+  });
+  
+  app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
+  });
