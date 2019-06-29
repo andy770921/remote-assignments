d1f27ffdade37636581238ed37a06033712f9b0d
Andy Chou <andy770921@yahoo.com.tw>
2019-06-30 00:54:09 +0800

    finish assingment 2

--- week3/Assignment1/app.js
+++ week3/Assignment1/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=> {
-    res.send('Hello, My Server!');
-  });
-  app.get('/getData', (req, res)=> {
+app.get('/', (req, res) => {
+  res.send('Hello, My Server!');
+});
+app.get('/getData', (req, res) => {
+  if (req.query.number === undefined) {
     res.send('Lack of Parameter');
-  });
-  
-  app.listen(3000, () => {
-    console.log('The application is running on localhost:3000');
-  });
+  }
+  else if (Number.isInteger(Number(req.query.number)) && Number(req.query.number) > 0) {
+    const ans = (1 + Number(req.query.number)) * Number(req.query.number) / 2;
+    res.send(String(ans));
+  } else if ((!Number.isInteger(Number(req.query.number)) || Number(req.query.number) <= 0)) {
+    res.send('Wrong Parameter');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000');
+});
